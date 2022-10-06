40772819cece3c8527f8f15e83631378a04e393a
beatendero <beatriz.tendero.hernandez@gmail.com>
2022-10-06 13:58:45 +0200

    dia 3

--- diia2/JV/main.js
+++ diia2/JV/main.js
@@ -94,66 +94,216 @@
 // despues te dirá por console log si tienes la edad o no para poder verla.
 
 
-let pelicula = prompt ("¿Que pelicula quieres ver?");
+// let pelicula = prompt ("¿Que pelicula quieres ver?");
 
-let edad = parseInt (prompt("¿ Qué edad tienes ?"));
+// let edad = parseInt (prompt("¿ Qué edad tienes ?"));
 
-let obejetoPeliculas ={
-    pelicula1:{
-     titulo : "el rey león",
-     edad: 3
+// let obejetoPeliculas ={
+//     pelicula1:{
+//      titulo : "el rey león",
+//      edad: 3
 
-    },
-    pelicula2:{
-        titulo: "soy legenda",
-        edad:13
-    },
+//     },
+//     pelicula2:{
+//         titulo: "soy legenda",
+//         edad:13
+//     },
 
-    pelicula3:{
-        titulo:"la marca del demonio",
-        edad:18
-    },
-    pelicula4:{
-        titulo:"a todo gas",
-        edad:8
-    },
+//     pelicula3:{
+//         titulo:"la marca del demonio",
+//         edad:18
+//     },
+//     pelicula4:{
+//         titulo:"a todo gas",
+//         edad:8
+//     },
 
-};
+// };
 
-switch (pelicula) {
-    case "el rey leon":
-        if (edad >= objetoPeliculas.pelicula1.edad) {
-            console.log("Puedes ver la película");
-        } else {
-            console.log("No puedes ver la película");
-        }
-        break;
-    case "soy leyenda":
-        if (edad >= objetoPeliculas.pelicula2.edad) {
-            console.log("Puedes ver la película");
-        } else {
-            console.log("No puedes ver la película");
-        }
-        break;
-    case "la marca del demonio":
-        if (edad >= objetoPeliculas.pelicula3.edad) {
-            console.log("Puedes ver la película");
-        } else {
-            console.log("No puedes ver la película");
-        }
-        break;
-    case "a todo gas":
-        if (edad >= objetoPeliculas.pelicula4.edad) {
-            console.log("Puedes ver la película");
-        } else {
-            console.log("No puedes ver la película");
-        }
-        break;
-    default:
-        console.log("Esa película no está disponible");
+// switch (pelicula) {
+//     case "el rey leon":
+//         if (edad >= objetoPeliculas.pelicula1.edad) {
+//             console.log("Puedes ver la película");
+//         } else {
+//             console.log("No puedes ver la película");
+//         }
+//         break;
+//     case "soy leyenda":
+//         if (edad >= objetoPeliculas.pelicula2.edad) {
+//             console.log("Puedes ver la película");
+//         } else {
+//             console.log("No puedes ver la película");
+//         }
+//         break;
+//     case "la marca del demonio":
+//         if (edad >= objetoPeliculas.pelicula3.edad) {
+//             console.log("Puedes ver la película");
+//         } else {
+//             console.log("No puedes ver la película");
+//         }
+//         break;
+//     case "a todo gas":
+//         if (edad >= objetoPeliculas.pelicula4.edad) {
+//             console.log("Puedes ver la película");
+//         } else {
+//             console.log("No puedes ver la película");
+//         }
+//         break;
+//     default:
+//         console.log("Esa película no está disponible");
 
-};
+// };
+
+//////////////////////////////////////////////////////////////////////////////////////
+//                               DIA 3
+////////////////////////////////////////////////////////////////////////////////////////
+
+// let dia = "Jueves";
+
+// var mes = "Octubre";
+
+// function Fecha(){
+    
+//     let dia = "Miercoles";
+//     var mes = "Noviembre";
+
+//     console.log(dia);
+//     // console.log(mes);
+
+// };
+
+// Fecha ();
+
+// console.log(dia);
+// // console.log(mes);
+
+////
+
+//FUNCTIONS VS ARROW-FUNCTIONS
+
+// function Multiplicar(numero1,numero2){
+//     return numero1 * numero2;
+// };
+
+// // Arrow-function
+
+// const Multiplicar = (numero1,numero2) =>{
+//     return numero1 * numero2;
+// };
+
+// const Dividir =(numero1, numero2) => numero1 / numero2;
+//let resultado = Dividir(10,3);
+
+//console.log(resultado);
+
+/// ES LO MISMO EL PRIMERO Y EL SEGUNDO///
+
+// function Exponente (numero){
+//     return numero **2;
+// }
+
+// const Exponente = numero => numero **2;
+
+////////////
+
+// let pokemon = {
+//     nombre :
+// }
+
+// const Pokemon = nombre =>({monster : nombre});
+
+// let boton = Pokemon ("charmander");
+
+// console.log(boton);
+
+// let fruta = ["manzana","pera","fresa","albaricoque","platano"];
+
+// let frutasPlurales = fruta.map(postre => postre + "s");
+
+// console.log(frutasPlurales);
+
+//
+//
+
+// let numeros = [25,38,42,57,112];
+
+// let numerosMapeados = numeros.map(cifra => (cifra * 3);
+    
+// console.log(numerosMapeados);
 
 
+//
 
+// let precioFinalImpuesto = (precio,impuesto = 0.21) => precio + (precio * impuesto);
+
+// console.log(precioFinalImpuesto(1200));
+////////////////////////////////////////////////////
+
+// const RobotRandom = (numero,letras ="RX")=> letras + numero;
+
+// let random = Math.floor(Math.random() * 999);
+
+// let resultado = RobotRandom(random);
+
+// console.log("Tu bot se llama ....",resultado);
+///////
+///
+//////
+//////////////
+// const espacir = (name,ps,atk,def,speed)=>{
+//     console.log("Nombre: ", name);
+//     console.log("Vida: ", ps);
+//     console.log("Ataque: ", atk);
+//     console.log("Defensa: ", def);
+//     console.log("velocidad: ", speed);
+// }
+
+// let Pokemon = ["Pikachu", 35,55,40,90]
+
+// espacir(...Pokemon)
+
+// ////////
+// ////////
+// function f(...args) {
+//     console.log(args);
+//     }
+//     f(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
+//     function g(arg1, arg2, ...argRest) {
+//     console.log(arg1, arg2, argRest);
+//     }
+//     g(1, 2, 3, 4, 5); // 1 2 [3,4,5]
+
+/////////////////////////////////////////////////////////////////
+
+// Se propone generar una funcion que usando REST, le pasamos varios argumentos 
+// correspondientes a las habilidades de un luchador o luchadora, y posteriormente
+//  las mostrará por console.log , a cada habilidad le sumará un valor random del 1 al 10.
+
+
+// const spread = (name, ps, atk, def, speed) => {
+//     console.log("Nombre: ", name);
+//     console.log("Vida: ", ps);
+//     console.log("Ataque: ", atk);
+//     console.log("Defensa: ", def);
+//     console.log("Velocidad: ", speed);
+//     }
+//     let Luchador = ["Deku", 75,82, 43, 90]
+//     spread(...Luchador)
+
+//
+    // const Agrupar = (...argumentos) => {
+    //     let aleatorio = (Math.random()* 10);
+
+    //     let nuevoArgumentos = argumentos.map (numeros=> numero + aleatorio)
+    // };
+
+    // Agrupar("fuerza 5", "velocidad 6", "vida 7","resistencia 11"); // [1, 2, 3, 4, 5]
+
+    ////////////////////
+
+//   const Pokemon = nombre=> ({nombre})
+
+// let  favorito = prompt("Dime tu compañero favorito")
+
+// console.log(`Hola ${favorito}`);
 
